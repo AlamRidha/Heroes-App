a632b446bdb2c7b3c8e3a678740b6df92c3ce1cd
AlamRidha <alamrridha@gmail.com>
2023-11-13 01:16:05 +0700

    Make Sticky Header and ViewModel

--- app/src/main/java/com/example/jetheroes/JetHeroesApp.kt
+++ app/src/main/java/com/example/jetheroes/JetHeroesApp.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -20,8 +21,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -29,20 +33,29 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
-import com.example.jetheroes.model.HeroesData
+import com.example.jetheroes.data.HeroRepository
 import com.example.jetheroes.ui.theme.JetHeroesTheme
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun JetHeroesApp(
     modifier: Modifier = Modifier,
+    viewModel: JetHeroesViewModel = viewModel(factory = ViewModelFactory(HeroRepository()))
 ) {
+//    val groupedHeroes = HeroesData.heroes.sortedBy { it.name }.groupBy { it.name[0] }
+    val groupedHeroes by viewModel.groupedHeroes.collectAsState()
+
+
     Box(modifier = modifier) {
         val scope = rememberCoroutineScope()
         val listState = rememberLazyListState()
@@ -53,13 +66,25 @@ fun JetHeroesApp(
             state = listState,
             contentPadding = PaddingValues(bottom = 80.dp)
         ) {
-            items(HeroesData.heroes, key = { it.id }) { hero ->
-                HeroListItem(
-                    name = hero.name,
-                    photoUrl = hero.photoUrl,
-                    modifier = Modifier.fillMaxWidth()
-                )
+            groupedHeroes.forEach { (initial, heroes) ->
+                stickyHeader {
+                    CharacterHeader(initial)
+                }
+                items(heroes, key = { it.id }) { hero ->
+                    HeroListItem(
+                        name = hero.name,
+                        photoUrl = hero.photoUrl,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
             }
+//            items(HeroesData.heroes, key = { it.id }) { hero ->
+//                HeroListItem(
+//                    name = hero.name,
+//                    photoUrl = hero.photoUrl,
+//                    modifier = Modifier.fillMaxWidth()
+//                )
+//            }
         }
         AnimatedVisibility(
             visible = showButton,
@@ -77,6 +102,27 @@ fun JetHeroesApp(
                 }
             )
         }
+    }
+}
+
+@Composable
+fun CharacterHeader(
+    char: Char,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier
+    ) {
+        Text(
+            text = char.toString(),
+            fontWeight = FontWeight.Black,
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
     }
 }
 
---
+++ app/src/main/java/com/example/jetheroes/JetHeroesViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.jetheroes
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.jetheroes.data.HeroRepository
+import com.example.jetheroes.model.Hero
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class JetHeroesViewModel(private val repository: HeroRepository) : ViewModel() {
+    private val _groupedHeroes = MutableStateFlow(
+        repository.getHeroes()
+            .sortedBy { it.name }
+            .groupBy { it.name[0] }
+    )
+    val groupedHeroes: StateFlow<Map<Char, List<Hero>>> get() = _groupedHeroes
+}
+
+class ViewModelFactory(private val repository: HeroRepository) :
+    ViewModelProvider.NewInstanceFactory() {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(JetHeroesViewModel::class.java)) {
+            return JetHeroesViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
+}
